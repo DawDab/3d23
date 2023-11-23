87e972366e8216f84dd863a78ae410ccbb79e37c
DawDab <d.dabrowski1999@gmail.com>
2023-11-23 22:44:27 +0100

    remove layout qualifiers and change var name

--- src/Assignments/Colors/shaders/base_fs.glsl
+++ src/Assignments/Colors/shaders/base_fs.glsl
@@ -1,7 +1,7 @@
 #version 410
 
 layout(location = 0) out vec4 vFragColor;
-layout(location = 1) in vec4 color; // Input color from vertex shader
+in vec4 color; // Input color from vertex shader
 
 void main() {
     vFragColor = color;
